57603c96ba47971f1a5fe4a42e992f552578c2d5
LevaniIlashvili <levaniko900@gmail.com>
2023-03-20 20:46:18 +0400

    added function to display shortened url

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 "use strict";
 
+const shortenUrl = document.querySelector(".btn-shorten-url");
+const inputUrl = document.querySelector(".input-url");
+const savedUrls = document.querySelector(".saved-urls");
+const errorContainer = document.querySelector(".error-container");
+
 async function shortenURL(url) {
   try {
     const res = await fetch(`https://api.shrtco.de/v2/shorten?url=${url}`);
@@ -13,3 +18,29 @@ async function shortenURL(url) {
     console.error(error);
   }
 }
+
+function addURLDom(originalLink, shortLink) {
+  const markup = `
+      <div class="url-container">
+        <p class="original-url">${originalLink}</p>
+        <div class="shortened-url">
+        <p>${shortLink}</p>
+          <button class="btn-cyan btn-copy">Copy</button>
+          </div>
+      </div>
+      `;
+  savedUrls.insertAdjacentHTML("beforebegin", markup);
+}
+
+shortenUrl.addEventListener("click", async (e) => {
+  try {
+    e.preventDefault();
+    errorContainer.textContent = "";
+    const inputLink = inputUrl.value;
+    const shortLink = await shortenURL(inputLink);
+    if (!shortLink) return;
+    addURLDom(inputLink, shortLink);
+  } catch (error) {
+    console.error(error);
+  }
+});
