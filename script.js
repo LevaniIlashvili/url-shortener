6d524649a8e56adec44ab8b63dfa5dd160552fb3
LevaniIlashvili <levaniko900@gmail.com>
2023-03-20 20:58:42 +0400

    added error messages

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 "use strict";
 
 const shortenUrl = document.querySelector(".btn-shorten-url");
-const inputUrl = document.querySelector(".input-url");
+const inputUrlDOM = document.querySelector(".input-url");
 const savedUrls = document.querySelector(".saved-urls");
 const errorContainer = document.querySelector(".error-container");
+
+const errorCodes = {
+  1: "Please add a link",
+  2: "Invalid link",
+  3: "Wait a second and try again",
+  4: "IP-Address has been blocked",
+  6: "Unknown error",
+  10: "Disallowed link",
+};
 
 async function shortenURL(url) {
   try {
     const res = await fetch(`https://api.shrtco.de/v2/shorten?url=${url}`);
     const data = await res.json();
     const result = data.result;
-    if (!data.ok) throw Error(data.error);
+
+    if (!data.ok) throw Error(errorCodes[data.error_code]);
+
     const { short_link: shortLink } = result;
     return shortLink;
   } catch (error) {
@@ -29,17 +40,18 @@ function addURLDom(originalLink, shortLink) {
           </div>
       </div>
       `;
-  savedUrls.insertAdjacentHTML("beforebegin", markup);
+  savedUrls.insertAdjacentHTML("afterbegin", markup);
 }
 
 shortenUrl.addEventListener("click", async (e) => {
   try {
     e.preventDefault();
     errorContainer.textContent = "";
-    const inputLink = inputUrl.value;
+    const inputLink = inputUrlDOM.value;
     const shortLink = await shortenURL(inputLink);
     if (!shortLink) return;
     addURLDom(inputLink, shortLink);
+    inputUrlDOM.value = "";
   } catch (error) {
     console.error(error);
   }
